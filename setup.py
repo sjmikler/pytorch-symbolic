56cc03f9ded38c1755311d9b2e211591f254e1c9
Szymon Mikler <sjmikler@gmail.com>
2022-10-02 02:14:12 +0200

    Update tests

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ print("Packages found:", packages)
 
 setup(
     name="pytorch-functional",
-    version="0.2.8",
+    version="0.2.9",
     url="https://github.com/gahaalt/pytorch-functional.git",
     project_urls={
         "Documentation": "https://pytorch-functional.readthedocs.io/",
