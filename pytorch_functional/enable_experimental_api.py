55cfc234f1f16fa59bb051daac75b5c5cc0a9cef
Szymon Mikler <sjmikler@gmail.com>
2022-10-07 21:14:35 +0200

    Sensible func names

--- pytorch_functional/enable_experimental_api.py
+++ pytorch_functional/enable_experimental_api.py
@@ -20,29 +20,35 @@ for value in layers.__dict__.values():
 
 logging.debug("DETECTING MODULES FINISHED")
 
-for module in predefined_modules:
 
-    def replace_call(module):
-        __old_call__ = module.__call__
-        module.__old_call__ = __old_call__
+def enable_functional_api_for_module(module):
+    __old_call__ = module.__call__
+    module.__old_call__ = __old_call__
 
-        def experimental_monkey_patch_call(self, *args, **kwds):
-            if (
-                len(args) > 0
-                and len(kwds) == 0
-                and all((isinstance(x, FMGraphNode) for x in args))
-            ):
-                node = args[0]
-                return node.apply_layer(self, *args[1:])
-            else:
-                local_value = module
-                return __old_call__(self, *args, **kwds)
+    def experimental_monkey_patch_call(self, *args, **kwds):
+        if (
+            len(args) > 0
+            and len(kwds) == 0
+            and all((isinstance(x, FMGraphNode) for x in args))
+        ):
+            node = args[0]
+            return node.apply_layer(self, *args[1:])
+        else:
+            return __old_call__(self, *args, **kwds)
 
-        module.__call__ = experimental_monkey_patch_call
-
-    replace_call(module)
+    module.__call__ = experimental_monkey_patch_call
 
 
-def disable_experimental_api():
+def enable_experimental_api_for_predefined_modules():
     for module in predefined_modules:
+        logging.debug(f"ENABLING EXPERIMENTAL API FOR {module}")
+        enable_functional_api_for_module(module)
+
+
+enable_experimental_api_for_predefined_modules()
+
+
+def disable_experimental_api_for_predefined_modules():
+    for module in predefined_modules:
+        logging.debug(f"DISABLING EXPERIMENTAL API FOR {module}")
         module.__call__ = module.__old_call__
