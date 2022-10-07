c135e1f16a28a489a880e627d3583cbbd86136b3
Szymon Mikler <sjmikler@gmail.com>
2022-10-08 00:47:58 +0200

    Isort

--- pytorch_functional/__init__.py
+++ pytorch_functional/__init__.py
@@ -1,4 +1,4 @@
-from .functional_model import FunctionalModel, Input
 from .experimental_api_v2 import enable_functional_api_for_new_modules
+from .functional_model import FunctionalModel, Input
 
 enable_functional_api_for_new_modules()
