60b8bfe6179e8e457b4e89f5a3a5986775875bda
Szymon Mikler <sjmikler@gmail.com>
2022-10-08 18:11:47 +0200

    Typo

--- pytorch_functional/__init__.py
+++ pytorch_functional/__init__.py
@@ -1,6 +1,6 @@
 #  Copyright (c) 2022 Szymon Mikler
 
-from .config import enable_module_call_optimization
+from .configs import enable_module_call_optimization
 from .functional_model import FunctionalModel, Input
 
 __all__ = ["enable_module_call_optimization", "FunctionalModel", "Input"]
