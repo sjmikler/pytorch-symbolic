5495d6125b01aaebc2f8f09a13909283fd003a09
Szymon Mikler <sjmikler@gmail.com>
2022-10-10 01:47:03 +0200

    Update notebook

--- pytorch_functional/__init__.py
+++ pytorch_functional/__init__.py
@@ -1,6 +1,7 @@
 #  Copyright (c) 2022 Szymon Mikler
 
+from . import tools
 from .configs import enable_module_call_optimization
 from .functional_model import FunctionalModel, Input
 
-__all__ = ["enable_module_call_optimization", "FunctionalModel", "Input"]
+__all__ = ["enable_module_call_optimization", "FunctionalModel", "Input", "tools"]
