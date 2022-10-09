89853bf402690321833f6ee0db3ca703644561ce
Szymon Mikler <sjmikler@gmail.com>
2022-10-09 17:24:40 +0200

    Add auto doc testing

---
+++ pytorch_functional/tests/test_docs_automated.py
@@ -0,0 +1,36 @@
+#  Copyright (c) 2022 Szymon Mikler
+
+import re
+import os
+import pathlib
+
+
+def scan_for_code_blobs(text):
+    blobs = re.findall(r"```py\n([\s\S]+?)\n```", text)
+    return [blob for blob in blobs if "..." not in blob]
+
+
+def test_all_code_blobs():
+    assert os.path.exists("docs")
+    all_code_blobs = []
+
+    for root, dirs, files in os.walk("docs"):
+        for file in files:
+            path = pathlib.Path(os.path.join(root, file))
+            if path.suffix == ".md":
+                code_blobs = scan_for_code_blobs(path.open("r").read())
+                for blob in code_blobs:
+                    all_code_blobs.append(blob)
+
+    for idx, blob in enumerate(all_code_blobs):
+        try:
+            globals_temp = {}
+            exec(blob, globals_temp)
+        except Exception as e:
+            print(f"Exception during automated documentation testing {idx}/{len(code_blobs)}.")
+            print(f"Source code that failed:")
+            print(blob)
+            raise e
+
+
+test_all_code_blobs()
