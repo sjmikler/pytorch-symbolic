3d0229edc96b05ae86a89af938ac38a20ad24f87
Szymon Mikler <sjmikler@gmail.com>
2022-10-10 18:47:36 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pytorch-functional",
-    version="0.4.12",
+    version="0.4.13",
     url="https://github.com/gahaalt/pytorch-functional.git",
     project_urls={
         "Documentation": "https://pytorch-functional.readthedocs.io/",
