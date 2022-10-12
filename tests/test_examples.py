fecaf1716f8143a51403c904d25665852664bd42
Szymon Mikler <sjmikler@gmail.com>
2022-10-12 18:15:25 +0200

    New tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,12 +1,13 @@
 #  Copyright (c) 2022 Szymon Mikler
 
 import torch
+from torch import nn
 
 import examples
-from pytorch_functional import model_tools
+from pytorch_functional import FunctionalModel, Input, model_tools
 
 
-def test_toy_resnet():
+def test_example_toy_resnet():
     model = examples.resnet.ToyResNet((3, 32, 32), n_classes=10)
     inputs = torch.rand(16, 3, 32, 32)
     outputs = model(inputs)
@@ -14,7 +15,7 @@ def test_toy_resnet():
     assert model_tools.get_parameter_count(model) == 175530
 
 
-def test_wrn():
+def test_example_wrn():
     model = examples.resnet.ResNet((3, 32, 32), n_classes=10, version=("WRN", 16, 4))
     inputs = torch.rand(16, 3, 32, 32)
     outputs = model(inputs)
@@ -22,7 +23,7 @@ def test_wrn():
     assert model_tools.get_parameter_count(model) == 2750698
 
 
-def test_vgg():
+def test_example_vgg():
     model = examples.vgg.VGG((3, 32, 32), n_classes=10, version=13)
     inputs = torch.rand(16, 3, 32, 32)
     outputs = model(inputs)
@@ -30,9 +31,33 @@ def test_vgg():
     assert model_tools.get_parameter_count(model) == 9413066
 
 
-def test_enc_dec():
+def test_example_enc_dec():
     model = examples.encoder_decoder.simple_encoder_decoder((3, 32, 32))
     inputs = torch.rand(16, 3, 32, 32)
     outputs = model(inputs)
     assert list(outputs.shape) == [16, 1, 7, 7]
     assert model_tools.get_parameter_count(model) == 28529
+
+
+def test_resnet():
+    inputs = Input(shape=(3, 32, 32))
+    x = nn.Conv2d(inputs.channels, 32, 3)(inputs)(nn.ReLU())
+    x = nn.Conv2d(x.channels, 64, 3)(x)(nn.ReLU())
+    block_1_output = nn.MaxPool2d(3)(x)
+
+    x = nn.Conv2d(block_1_output.channels, 64, 3, padding=1)(block_1_output)(nn.ReLU())
+    x = nn.Conv2d(x.channels, 64, 3, padding=1)(x)(nn.ReLU())
+    block_2_output = x + block_1_output
+
+    x = nn.Conv2d(block_2_output.channels, 64, 3, padding=1)(block_2_output)(nn.ReLU())
+    x = nn.Conv2d(x.channels, 64, 3, padding=1)(x)(nn.ReLU())
+    block_3_output = x + block_2_output
+
+    x = nn.Conv2d(x.channels, 64, 3)(block_3_output)(nn.ReLU())
+    x = nn.AvgPool2d(kernel_size=(x.H, x.W))(x)(nn.Flatten())
+    x = nn.Linear(x.features, 256)(x)(nn.ReLU())
+    x = nn.Dropout(0.5)(x)
+    outputs = nn.Linear(x.features, 10)(x)
+
+    model = FunctionalModel(inputs, outputs)
+    assert model_tools.get_parameter_count(model) == 223242
