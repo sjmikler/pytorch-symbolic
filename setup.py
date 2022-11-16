eb85e9d73fd834ac82834e0ef0fe77e89de5e59c
Szymon Mikler <sjmikler@gmail.com>
2022-11-16 14:29:52 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pytorch-symbolic",
-    version="1.0.1",
+    version="1.0.2",
     url="https://github.com/gahaalt/pytorch-symbolic.git",
     project_urls={
         "Documentation": "https://pytorch-symbolic.readthedocs.io/",
